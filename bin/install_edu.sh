7787603865faf4e9b295f87a45503f683f52713a
Ken Wakita <wakita@is.titech.ac.jp>
2011-05-21 23:42:47 +0900

    TSUBAMEにインストールしたときにデバッグモードを外すことにした．

--- bin/install_edu.sh
+++ bin/install_edu.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 scp document.php index.html typing.css typing.js tsubame:edu/public_html/apps/typing
-ssh tsubame 'cd edu/public_html/apps/typing; sed -e "s|/lib/jquery-1.4.4.js|/~wakita-k-aa/lib/jquery-1.4.4.js|" index.html > tmp; mv tmp index.html'
+ssh tsubame 'cd edu/public_html/apps/typing; sed -e "s|/lib/jquery-1.4.4.js|/~wakita-k-aa/lib/jquery-1.4.4.js|" index.html > tmp; mv tmp index.html; sed -e "s/debug = true/debug = false/" typing.js > tmp; mv tmp typing.js'
